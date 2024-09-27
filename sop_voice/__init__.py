03aa241337110755744526fc9aedacaeae144379
Leorevoir <leoquinzler@epitech.eu>
2024-09-27 14:18:32 +0200

    update: version v.1.1.9

--- sop_voice/__init__.py
+++ sop_voice/__init__.py
@@ -5,7 +5,7 @@ class SopVoiceConfig(PluginConfig):
     name = "sop_voice"
     verbose_name = "SOP Voice"
     description = "Manage voice informations of each site."
-    version = "1.1.8"
+    version = "1.1.9"
     author = "Leorevoir"
     author_email = "leoquinzler@epitech.eu"
     base_url = "sop-voice"
