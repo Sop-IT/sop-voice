79e67e8673da8f4b4d9219a137cc31790a8df4c2
Leorevoir <leoquinzler@epitech.eu>
2024-09-27 11:55:25 +0200

    feat: update plugin version to 1.1.7

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-voice',
-    version='1.1.6',
+    version='1.1.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'sop-utils>=1.0.7',
+        'phonenumbers'
     ],
     description="Manage voice informations of each sites.",
     author="Leorevoir",
