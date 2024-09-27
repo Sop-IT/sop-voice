81b50281e94d2ed982f29a06e8c5f20ed9a661e3
Leorevoir <leoquinzler@epitech.eu>
2024-09-27 17:28:08 +0200

    version 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-voice',
-    version='1.2.1',
+    version='1.2.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
