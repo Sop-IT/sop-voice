1ff95cfc9c5acf75493e7b7f0f6d4cf780f47005
tibobo67 <48986982+tibobo67@users.noreply.github.com>
2024-10-01 18:16:54 +0200

    remove unused import

--- sop_voice/models.py
+++ sop_voice/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models import Transform
-from django.db.models.functions import math 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
