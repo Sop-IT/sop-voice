71e157db3216f06dd10340ba876d03abf33bb7b3
Leorevoir <leoquinzler@epitech.eu>
2024-10-02 10:57:30 +0200

    add: length constraint migration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-voice',
-    version='1.2.6',
+    version='1.2.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
