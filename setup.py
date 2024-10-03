a00a10411a0fcbb8e95e8f439efcd6ab7829fcb1
Leorevoir <leoquinzler@epitech.eu>
2024-10-03 15:21:37 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-phone',
-    version='0.2.3',
+    version='0.2.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
