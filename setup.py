29e143c9fe75e3824991be553adcfe0cc9626cc3
Leorevoir <leo.quinzler@epitech.eu>
2024-10-18 16:33:25 +0200

    add: v.0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sop-phone',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
