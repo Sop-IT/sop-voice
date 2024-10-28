cdb0c78ef982ae573cfc02d759c5f6b69f634e79
Leorevoir <leo.quinzler@epitech.eu>
2024-10-28 12:14:23 +0100

    feat: deploy-script auto source to venv

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 export TWINE_USERNAME="$pypi_name"
 export TWINE_PASSWORD="$phone_api"
 
+source venv/bin/activate
 python3 setup.py sdist bdist_wheel
 twine upload --skip-existing --verbose dist/*
 
