27d4e5c37aafad4789b3ebb5bc4f2222fb457715
Leorevoir <leo.quinzler@epitech.eu>
2024-11-04 16:19:40 +0100

    add: v.0.4.2

--- sop_phone/__init__.py
+++ sop_phone/__init__.py
@@ -5,7 +5,7 @@ class SopPhoneConfig(PluginConfig):
     name = "sop_phone"
     verbose_name = "SOP Phone"
     description = "Manage phone informations of each site."
-    version='0.4.1'
+    version='0.4.2'
     author = "Leorevoir"
     author_email = "leoquinzler@epitech.eu"
     base_url = "sop-phone"
